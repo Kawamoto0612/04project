27e9078c5f9913ab97f59f8e72a5f38ff4046f27
Kawamoto <.gitkawamoto.izumi@gmail.com>
2025-09-21 16:56:38 +0900

    第5回演習課題です

---
+++ Student.management/src/main/java/student/Student/management/Application.java
@@ -0,0 +1,42 @@
+package student.Student.management;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	private Map<String, String> studentData = new HashMap<>();
+
+	public static void main(String[] args) {
+			SpringApplication.run(Application.class, args);
+	}
+
+	@GetMapping("/students")
+	public Map<String, String> getAllStudents(){
+		return studentData;
+	}
+
+	@PostMapping("/addStudent")
+	public String addStudent(@RequestParam String name, @RequestParam String age){
+		studentData.put(name,age);
+	 return name +"さんを追加しました（年齢："+ age +"歳）";
+}
+
+ @PostMapping("/updateStudent")
+	public  String updateStudent(@RequestParam String name,@RequestParam String age) {
+		if (studentData.containsKey(name)){
+			studentData.put(name,age);
+			return name + "さんの年齢を" + age + "歳に更新しました";
+		} else 	{
+			return name + "さんは存在しません";
+		}
+ }
+}
